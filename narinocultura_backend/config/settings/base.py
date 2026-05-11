000c2bedc15eb7e45b9f9b9bdeff9ef7a8d0466e
Johan Delgado <134401808+JohanDsito@users.noreply.github.com>
2026-05-10 23:58:01 -0500

    allowed host para permitir url

--- narinocultura_backend/config/settings/base.py
+++ narinocultura_backend/config/settings/base.py
@@ -9,7 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = config("SECRET_KEY", default="unsafe-secret-key-change-me")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config("ALLOWED_HOSTS", default="*", cast=Csv())
+ALLOWED_HOSTS = config(
+    "ALLOWED_HOSTS",
+    default="localhost,127.0.0.1,narinocultura-production.up.railway.app",
+    cast=Csv(),
+)
 
 INSTALLED_APPS = [
     "django.contrib.admin",
