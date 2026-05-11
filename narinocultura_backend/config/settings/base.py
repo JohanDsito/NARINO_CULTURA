cec6568347f076b374646a144f5c29dad58edffe
Johan Delgado <134401808+JohanDsito@users.noreply.github.com>
2026-05-11 00:06:53 -0500

    cambio de cadena vacia en allowe_host

--- narinocultura_backend/config/settings/base.py
+++ narinocultura_backend/config/settings/base.py
@@ -9,11 +9,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 SECRET_KEY = config("SECRET_KEY", default="unsafe-secret-key-change-me")
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = config(
+ALLOWED_HOSTS = [host for host in config(
     "ALLOWED_HOSTS",
     default="",
     cast=Csv(),
-)
+) if host]
 if not ALLOWED_HOSTS:
     ALLOWED_HOSTS = [
         "localhost",
