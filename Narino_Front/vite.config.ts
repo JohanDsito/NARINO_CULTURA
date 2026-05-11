41e75566377f5c5ccc7f119383c673be5c36a928
Juan Manuel cabrera <127572806+juanma2025@users.noreply.github.com>
2026-05-10 23:57:18 -0500

    configuración de vitest y jest, primera prueba realizada

--- Narino_Front/vite.config.ts
+++ Narino_Front/vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
@@ -33,4 +33,23 @@ export default defineConfig({
       },
     },
   },
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+
+    setupFiles: './src/test/setup.ts',
+
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
+  },
 })
+
+
+
+
+
+
+
