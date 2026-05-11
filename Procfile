a47cf4ecda2f07348bedf2845ae3d4d0f8c1946f
Johan Delgado <134401808+JohanDsito@users.noreply.github.com>
2026-05-10 23:30:10 -0500

    eliminacion requirimientos innecesarios

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: cd narinocultura_backend && gunicorn config.wsgi --bind 0.0.0.0:$PORT
+worker: cd narinocultura_backend && python manage.py runworker channels
--- narinocultura_backend/config/settings/production.py
+++ narinocultura_backend/config/settings/production.py
@@ -4,3 +4,9 @@ DEBUG = False
 if SECRET_KEY == "unsafe-secret-key-change-me":
     raise RuntimeError("SECRET_KEY no configurada para producción.")
 
+# Configuraciones para producción
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
