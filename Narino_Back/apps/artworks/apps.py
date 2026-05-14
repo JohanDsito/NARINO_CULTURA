22f517dd13c419833a78aac2747f73a179c4353a
Johan D <delgadodelgad12@gmail.com>
2026-05-14 17:17:47 -0500

    eliminacion carpeta duplicada

--- Narino_Back/apps/artworks/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class ArtworksConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apps.artworks"
-    label = "artworks"
-
