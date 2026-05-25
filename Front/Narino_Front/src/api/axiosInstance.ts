8ff38ab617c58a81efc6c6f8fc580ddc3d71e76b
Johan Delgado <134401808+JohanDsito@users.noreply.github.com>
2026-05-25 02:24:33 -0500

    headers

--- Front/Narino_Front/src/api/axiosInstance.ts
+++ Front/Narino_Front/src/api/axiosInstance.ts
@@ -4,7 +4,6 @@ import { API_BASE_URL } from '@/config/env'
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
   timeout: 15000,
 })
 
